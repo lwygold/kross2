48b598e717db6030f2efac684289a881ff906f0d
이원영 <lwy@iwon-yeong-ui-MacBookPro.local>
2023-09-22 10:00:53 +0900

    Updates

---
+++ _next/static/chunks/app/about/page-a71c8ac266de0772.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7301],{9936:function(n,e,t){Promise.resolve().then(t.t.bind(t,6685,23)),Promise.resolve().then(t.t.bind(t,2541,23))},2541:function(){}},function(n){n.O(0,[6685,2971,596,1744],function(){return n(n.s=9936)}),_N_E=n.O()}]);
